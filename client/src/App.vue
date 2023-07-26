c433d77781b0a1da06e0683c1d203d4b4a2f4fe0
visakha sharma <139097523+vsharma2023@users.noreply.github.com>
2023-07-26 15:42:10 +0530

    Update App.vue

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,6 +1,11 @@
 <template>
   <v-app>
-    <v-main>
+    <v-main :style="{
+      '--v-layout-left': '0px',
+      '--v-layout-right': '0px',
+      '--v-layout-top': '0px',
+      '--v-layout-bottom': '0px',
+    }">
       <router-view/>
     </v-main>
   </v-app>
