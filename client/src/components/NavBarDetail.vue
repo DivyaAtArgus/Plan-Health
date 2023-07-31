a37be97b002f908ecf8c7d219314695cb7f30e0d
DivyaAtArgus <138983714+DivyaAtArgus@users.noreply.github.com>
2023-07-31 15:51:55 +0530

    Update NavBarDetail.vue

--- client/src/components/NavBarDetail.vue
+++ client/src/components/NavBarDetail.vue
@@ -8,33 +8,7 @@
   
       <v-spacer></v-spacer>
       <v-btn text @click="reserve" to="/" class="text-yellow">Home</v-btn>
-      <div class="d-flex justify-space-around">
-        <v-menu
-      transition="slide-x-transition"
-    >
-      <template v-slot:activator="{ props }">
-        <v-btn
-          color="white"
-          v-bind="props"
-        >
-          Departments
-        </v-btn>
-      </template>
-
-      <v-list>
-        <v-list-item
-          v-for="(department, i) in departments"
-          :key="i"
-        >
-          <v-list-item-title>{{ department.name }}</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-menu>
-      </div>
-      <v-btn >
-        <v-icon color="black" left class="mr-2"><i class="fa fa-sign-in" aria-hidden="true"></i></v-icon> 
-        Login
-      </v-btn>
+      
     </v-app-bar>
 </template>
 <script>
